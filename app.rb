c7dbb16a0c804d6b7fabcd6a36c0f863902fae61
Rémi Prévost <remi@exomel.com>
2010-03-21 07:06:44 -0400

    Add CSS examples

--- app.rb
+++ app.rb
@@ -14,13 +14,33 @@ class App < Sinatra::Base
     mustache :index
   end
 
+  get '/css/text-shadow' do
+    @title = 'Text-shadow - CSS — HTML5'
+    mustache :css_textshadow
+  end
+
+  get '/css/transform' do
+    @title = 'Transformations - CSS — HTML5'
+    mustache :css_transform
+  end
+
+  get '/css/border-radius' do
+    @title = 'Border-radius - CSS — HTML5'
+    mustache :css_borderradius
+  end
+
+  get '/css/box-shadow' do
+    @title = 'Box-shadow - CSS — HTML5'
+    mustache :css_boxshadow
+  end
+
   get '/javascript/geolocation' do
-    @title = 'Géolocalisation'
+    @title = 'Géolocalisation - Javascript — HTML5'
     mustache :javascript_geolocation
   end
 
   get '/javascript/storage' do
-    @title = 'Stockage local'
+    @title = 'Stockage local - Javascript — HTML5'
     mustache :javascript_storage
   end
 
